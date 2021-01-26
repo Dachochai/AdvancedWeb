2fcd5952f3a2c8b067df7f0cbc317a43b37b8ad1
Dachochai <dachochai69@gmail.com>
2021-01-26 12:42:00 +0700

    update

--- Lecture-02/HelloCloud.py
+++ Lecture-02/HelloCloud.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "Hello Dachochai from Server Start"
+    return "Hello Dachochai from Server Start 5555"
 
 if __name__ == "__main__":
     server.run(host='0.0.0.0', port=80)
