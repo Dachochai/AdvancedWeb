bed9ad718913cb65cb63d3b23e2f8c23e28a41b3
Dachochai <dachochai69@gmail.com>
2021-01-31 23:52:31 +0700

    ียกฟะำ

--- Lecture-03/HelloCloudDB.py
+++ Lecture-03/HelloCloudDB.py
@@ -1,16 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-#Init app
 app = Flask(__name__)
 
-#Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://webadmin:GXTlhs77128@node8572-advweb-10.app.ruk-com.cloud:11093/CloudDB'
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-#Init db
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://webadmin:GXTlhs77128@node8572-advweb-10.app.ruk-com.cloud/CloudDB'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
-#Init ma
+
 ma = Marshmallow(app)
 
 #Staff Class/Model
@@ -26,6 +24,52 @@ class Staffs(db.Model):
         self.email = email
         self.phone = phone
 
+# Create a Staff
+@app.route('/staff', methods=['POST'])
+def add_staff():
+    id = request.json['id']
+    name = request.json['name']
+    email = request.json['email']
+    phone = request.json['phone']
+
+    new_staff = Staffs(id, name, email, phone)
+
+    db.session.add(new_staff)
+    db.session.commit()
+
+    return staff_schema.jsonify(new_staff)
+
+
+
+# Update a Staff
+@app.route('/staff/<id>', methods=['PUT'])
+def update_staff(id):
+    staff = Staffs.query.get(id)
+    
+    name = request.json['name']
+    email = request.json['email']
+    phone = request.json['phone']
+
+    staff.name = name
+    staff.email = email
+    staff.phone = phone
+
+    db.session.commit()
+
+    return staff_schema.jsonify(staff)
+
+
+
+# Delete Staff
+@app.route('/staff/<id>', methods=['DELETE'])
+def delete_staff(id):
+    staff = Staffs.query.get(id)
+    db.session.delete(staff)
+    db.session.commit()
+    
+    return staff_schema.jsonify(staff)
+
+
 # Staff Schema
 class StaffSchema(ma.Schema):
     class Meta:
@@ -35,17 +79,16 @@ class StaffSchema(ma.Schema):
 staff_schema = StaffSchema()
 staffs_schema = StaffSchema(many=True)
 
-# Get All Staffs
+# Web Root Hello
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({'ms': 'Hello Cloud Patcharapong'})
+
 @app.route('/staffs', methods=['GET'])
 def get_staffs():
     all_staffs = Staffs.query.all()
     result = staffs_schema.dump(all_staffs)
     return jsonify(result)
-
-# Web Root Hello
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({'ms': 'Hello Cloud DB'})
 
 # Run Server
 if __name__ == "__main__":
